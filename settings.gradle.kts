df3749091f477d6a3117c480df3a55b5d375aede
Florent TOMI <florent.tomi@pytheasnavigation.com>
2024-11-22 16:17:59 +0100

    Initial project structure: * Gradle project * Publish to Repsy

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+
+        maven {
+            url = uri("https://maven.neoforged.net/releases")
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "bulkit-api"
